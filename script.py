73503fcaeb42acb9c8993b0006d96c72ddbb2ce3
unknown <duleba.pio@gmail.com>
2020-05-10 21:58:47 +0200

    hakuna matata

--- script.py
+++ script.py
@@ -2,32 +2,10 @@ import gdelt
 import pandas as pd
 # Version 1 queries
 gd1 = gdelt.gdelt(version=1)
-
-
 results= gd1.Search('2020-05-08',table='events',output='pd') # to już jest DataFrame
 
 
-#liczba rodzajów wydarzeń w danym datasecie, biorę pod uwagę 2 pierwsze cyfry kodu CAMEO
-# 01 - MAKE PUBLIC STATEMENT
-# 02 - APPEAL
-# 03 - EXPRESS INTENT TO COOPERATE
-# 04 - CONSULT
-# 05 - ENGAGE IN DIPLOMATIC COOPERATION
-# 06 - ENGAGE IN MATERIAL COOPERATION
-# 07 - PROVIDE AID
-# 08 - YIELD
-# 09 - INVESTIGATE
-# 10 - DEMAND
-# 11 - DISAPPROVE
-# 12 - REJECT
-# 13 - THREATEN
-# 14 - PROTEST
-# 15 - EXHIBIT FORCE POSTURE
-# 16 - REDUCE RELATIONS
-# 17 - COERCE
-# 18 - ASSAULT
-# 19 - FIGHT
-# 20 - USE UNCONVENTIONAL MASS VIOLENCE
+#zwraca liczbe rodzajów wydarzeń w danym datasecie, biorę pod uwagę 2 pierwsze cyfry kodu CAMEO
 def count_events(data, event_type):
     if not 1<=event_type<=20:
         return 0
@@ -36,24 +14,28 @@ def count_events(data, event_type):
     filtered = events.apply(lambda x:  int(x['EventCode'][:2]) , axis=1) 
     checked = filtered.apply(lambda x: 1 if x == event_type else 0 )
     return checked.sum()
-
+#print(count_events(results,7))
 
 
 # zwraca datafame z ilościa i % występowania poszczególnych typów zdarzeń w danym datasecie
 def event_types_ratio(data):
-    sum = data.shape[0]
+    sum = data.shape[0] # no bo tyle jest wszystkich zdarzeń co rekordów
     dictionary = {"event_type_cameo": [], "count": [], "ratio": []}
     for x in range (1,21):
         count = count_events(data, x)
         dictionary["event_type_cameo"].append(x)
         dictionary["count"].append(count)
         dictionary["ratio"].append(count/sum)
-        
-
     result = pd.DataFrame(data=dictionary)
     result.set_index('event_type_cameo')
     return result
+#print(event_types_ratio(results))
 
 
-
-print(event_types_ratio(results))
+# zwraca dataframe z współrzędnymi geograficznymi wydarzeń zachocących pomiędzy daną parą aktorów (podajemy ich kody CAMEO)
+def actors_action_geo(data, cameo_1, cameo_2):
+    results = data[["Actor1Code","Actor2Code","ActionGeo_Lat","ActionGeo_Long"]]
+    filter1 = results["Actor1Code"]==cameo_1
+    filter2 = results["Actor2Code"]==cameo_2
+    return results[filter1 & filter2]
+# print(actors_action_geo(results,"scoGOV",'GBR' ))
