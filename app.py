407d53c8ad9b3b8bf6c605a4c93cc24ea0d35da7
Jan <jan.ilczuk@gmail.com>
2020-05-24 20:57:20 +0200

    Enable CORS

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import pandas as pd
 import numpy as np
 import gdelt
 import script as scripts
+from flask_cors import CORS
 
 app = Flask(__name__)
 
 gd1 = gdelt.gdelt(version=1)
+
+CORS(app, resources={r'/*': {'origins': '*'}})
 
 @app.route('/')
 def hello():
