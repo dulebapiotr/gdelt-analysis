f0ecd9159193d4d3c4f15fb23f21b81b65ec5f2d
wiczek2 <wiczek2@poczta.onet.pl>
2020-05-25 17:18:41 +0200

    add route actors-actions-geo

--- app.py
+++ app.py
@@ -27,6 +27,21 @@ def dataframe():
     data = gd1.Search(date, table='events', output='pd')
     return data.to_json()
 
+@app.route('/actors-action-geo', methods=['GET'])
+def actors_action_geo():
+    data = request.get_json()
+    start = data.get('start')[0:10]  # possible the dumbest way to format a date, but whatevah. Normally it would be '2020-05-25 06:04:30,773' so i cut it short
+    stop = data.get('stop')[0:10]
+    actor1 = data.get('actor1')
+    actor2 = data.get('actor2')
+    if start == stop:
+        date = start
+    else:
+        date = [start, stop]
+    data = gd1.Search(date, table='events', output='pd')
+    result = scripts.actors_action_geo(data, actor1, actor2)
+    return result.to_json()
+
 
 if __name__ == '__main__':
     app.run()
